587ef1d542ecbd334774893a684859477c418ed4
chonhenry <chonhenry@gmail.com>
2023-05-20 17:16:04 -0400

    dark/light mode button in navbar

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,5 @@ import { useTheme } from "next-themes";
 export default function Home() {
   const { theme, setTheme } = useTheme();
 
-  return (
-    <main className="flex flex-col items-center h-screen">
-      <h1 className="text-black dark:text-white">hello world</h1>
-      <hr />
-      The current theme is: {theme}
-      <button onClick={() => setTheme("light")}>Light Mode</button>
-      <button onClick={() => setTheme("dark")}>Dark Mode</button>
-      <div className="w-10 h-10 bg-background-dark"></div>
-      <div className="w-10 h-10 bg-primary-dark"></div>
-      <div className="w-10 h-10 bg-secondary-dark"></div>
-      <div className="w-10 h-10 bg-primary-light"></div>
-      <div className="w-10 h-10 bg-secondary-light"></div>
-    </main>
-  );
+  return <main className="flex flex-col items-center h-screen"></main>;
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,26 +2,32 @@
 
 import { useTheme } from "next-themes";
 import { TfiWrite } from "react-icons/tfi";
+import { BsSun } from "react-icons/bs";
+import { TbMoonStars } from "react-icons/tb";
 
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
 
+  const toggleTheme = () => {
+    if (theme === "light") setTheme("dark");
+    else setTheme("light");
+  };
+
   return (
-    <nav className="max-w-[1400px] mx-auto py-6 px-8 flex justify-between relative">
+    <nav className="max-w-[1400px] mx-auto py-6 px-8 flex justify-between">
       <div className="flex items-center text-lg cursor-pointer text-primary-dark dark:text-secondary-light">
         <span className="font-semibold mr-3">
           <TfiWrite />
         </span>
-        <span className="font-semibold">Taxanomy</span>
+        <span className="font-semibold">NoteEditor</span>
       </div>
-      <div className="bg-secondary-light dark:bg-secondary-dark cursor-pointer text-sm rounded-lg py-2 px-5">
-        Login
-      </div>
-      <div className="absolute start-[170px]">
-        <button className="mr-3" onClick={() => setTheme("light")}>
-          Light
-        </button>
-        <button onClick={() => setTheme("dark")}>Dark</button>
+      <div className="flex items-center">
+        <div className="cursor-pointer mr-5 text-2xl" onClick={toggleTheme}>
+          {theme === "light" ? <BsSun /> : <TbMoonStars />}
+        </div>
+        <div className="bg-secondary-light dark:bg-secondary-dark cursor-pointer text-sm rounded-lg py-2 px-5">
+          Login
+        </div>
       </div>
     </nav>
   );

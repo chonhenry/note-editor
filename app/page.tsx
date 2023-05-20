6c5a1bc001f155bd76241603a6cd5b6887b8020e
chonhenry <chonhenry@gmail.com>
2023-05-20 19:38:26 -0400

    pricing plans

--- app/page.tsx
+++ app/page.tsx
@@ -50,12 +50,12 @@ export default function Home() {
         </button>
       </section>
 
-      <section className="text-center py-20 sm:py-32 px-10 mx-auto max-w-[1150px]">
+      <section className="text-center pb-20 mt-10 sm:mt-0 sm:pb-32 px-10 mx-auto max-w-[1150px]">
         <h1 className="text-xl font-semibold sm:text-2xl md:text-5xl mb-5 dark:text-primary-light">
           Features & Tech Stack
         </h1>
 
-        <p className="md:mx-44 text-l dark:text-secondary-light">
+        <p className="md:mx-44 text-l text-secondary-light">
           This website is a personal project that was built in my own interest.
           I built this project with the purpose of expanding my knowledge and
           skillsets as a software engineer and to learn new technologies.
@@ -72,6 +72,50 @@ export default function Home() {
           ))}
         </div>
       </section>
+
+      <section className="text-center pb-20 sm:pb-32 px-10 mx-auto max-w-[1150px]">
+        <h1 className="text-xl font-semibold sm:text-2xl md:text-5xl mb-5 dark:text-primary-light">
+          Pricing Plans
+        </h1>
+        <p className="text-secondary-light">
+          Choose a plan based on your needs.
+        </p>
+        <div className="w-full mt-5 flex flex-col items-center sm:flex-row sm:justify-center sm:items-stretch">
+          <div className="text-left p-5 w-full mb-5 sm:mb-0 sm:mr-10 border rounded-lg dark:border-secondary-light flex flex-col">
+            <div className="text-xl font-semibold mb-2">Free</div>
+            <ul className="text-sm text-secondary-light list-disc flex-auto">
+              <li className="ms-3">
+                Basic note creation and editing: Write and save simple notes.
+              </li>
+              <li className="ms-3">Limited storage: Store up to 5 notes.</li>
+            </ul>
+            <button className="bg-primary-dark text-primary-light dark:bg-primary-light dark:text-primary-dark w-full rounded-md mt-5 py-2">
+              Choose
+            </button>
+          </div>
+
+          <div className="text-left p-5 w-full mb-5 sm:mb-0 border rounded-lg dark:border-secondary-light flex flex-col">
+            <div className="text-xl font-semibold mb-2">Free</div>
+            <ul className="text-sm text-secondary-light list-disc flex-auto">
+              <li className="ms-3">All Free Plan Features</li>
+              <li className="ms-3">
+                Unlimited storage: Store an unlimited number of notes.
+              </li>
+              <li className="ms-3">
+                Advanced note creation and editing: Use rich text formatting,
+                insert images, and more.
+              </li>
+            </ul>
+            <button className="bg-primary-dark text-primary-light dark:bg-primary-light dark:text-primary-dark w-full rounded-md mt-5 py-2">
+              Choose
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <footer className="text-secondary-light text-center p-5">
+        The source code is available on my GitHub.
+      </footer>
     </main>
   );
 }
